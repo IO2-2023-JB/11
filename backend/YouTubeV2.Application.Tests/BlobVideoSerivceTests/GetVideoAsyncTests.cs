f3a7cf4d04b3697635d37aa6a9bae102914573fc
Marcin <marcin.latoszek06@gmail.com>
2023-03-24 23:26:54 +0100

    added test for typical case

---
+++ backend/YouTubeV2.Application.Tests/BlobVideoSerivceTests/GetVideoAsyncTests.cs
@@ -0,0 +1,61 @@
+﻿using Azure;
+using Azure.Storage.Blobs;
+using FluentAssertions;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Text;
+using YouTubeV2.Application.Configurations.BlobStorage;
+using YouTubeV2.Application.Services.AzureServices.BlobServices;
+
+namespace YouTubeV2.Application.Tests.BlobVideoSerivceTests
+{
+    [TestClass]
+    public class GetVideoAsyncTests
+    {
+        private Mock<BlobServiceClient> _blobServiceClientMock = new Mock<BlobServiceClient>();
+        private Mock<BlobContainerClient> _blobContainerClientMock = new Mock<BlobContainerClient>();
+        private Mock<BlobClient> _blobClientMock = new Mock<BlobClient>();
+        private BlobVideoService _blobVideoService = null!;
+        private byte[] _entireStreamBuffer = null!;
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _entireStreamBuffer = Encoding.UTF8.GetBytes("testStreamContent");
+            _blobClientMock
+                .Setup(x => x.DownloadToAsync(It.IsAny<Stream>(), It.IsAny<CancellationToken>()))
+                .Callback<Stream, CancellationToken>(async (stream, cancellationToken) =>
+                {
+                    await stream.WriteAsync(_entireStreamBuffer, 0, _entireStreamBuffer.Length, cancellationToken);
+                    await stream.FlushAsync(cancellationToken);
+                });
+            _blobClientMock
+                .Setup(x => x.ExistsAsync(It.IsAny<CancellationToken>()))
+                .ReturnsAsync(Response.FromValue(true, new Mock<Response>().Object));
+            _blobContainerClientMock.Setup(x => x.GetBlobClient(It.IsAny<string>())).Returns(_blobClientMock.Object);
+            _blobServiceClientMock
+                .Setup(x => x.GetBlobContainerClient(It.IsAny<string>()))
+                .Returns(_blobContainerClientMock.Object);
+            _blobVideoService = new BlobVideoService(
+                _blobServiceClientMock.Object,
+                Options.Create(new BlobStorageVideosConfig { VideosContainerName = It.IsAny<string>() }));
+        }
+
+        [TestMethod]
+        public async Task GetVideoTryingToGetBytesStartingAndEndingInRangeOfStreamShouldReturnFullBuffer()
+        {
+            // ARRANGE
+            System.Range range = new System.Range(1, 4);
+
+            // ACT
+            byte[] buffer = await _blobVideoService.GetVideoAsync(It.IsAny<string>(), range);
+
+            // ASSERT
+            buffer.Length.Should().Be(range.End.Value - range.Start.Value + 1);
+            for (int i = range.Start.Value; i <= range.End.Value; i++)
+                buffer[i - range.Start.Value].Should().Be(_entireStreamBuffer[i]);
+        }
+    }
+}
--- backend/YouTubeV2.Application/Services/AzureServices/BlobServices/BlobVideoService.cs
+++ backend/YouTubeV2.Application/Services/AzureServices/BlobServices/BlobVideoService.cs
@@ -1,5 +1,6 @@
 ﻿using Azure.Storage.Blobs;
 using Microsoft.Extensions.Options;
+using System.IO;
 using YouTubeV2.Application.Configurations.BlobStorage;
 
 namespace YouTubeV2.Application.Services.AzureServices.BlobServices
@@ -15,16 +16,21 @@ namespace YouTubeV2.Application.Services.AzureServices.BlobServices
             _blobStorageConfig = blobStorageConfig.Value;
         }
 
-        public async Task<byte[]> GetVideo(string fileName, Range bytesRange, CancellationToken cancellationToken = default)
+        public async Task<byte[]> GetVideoAsync(string fileName, Range bytesRange, CancellationToken cancellationToken = default)
         {
             BlobContainerClient blobContainerClient = _blobServiceClient.GetBlobContainerClient(_blobStorageConfig.VideosContainerName);
             BlobClient blobClient = blobContainerClient.GetBlobClient(fileName);
 
-            if (!(await blobClient.ExistsAsync()).Value)
+            if (!(await blobClient.ExistsAsync(cancellationToken)).Value)
                 throw new FileNotFoundException($"There is no video with fileName {fileName}");
 
             using var memoryStream = new MemoryStream();
-            await blobClient.DownloadToAsync(memoryStream);
+            await blobClient.DownloadToAsync(memoryStream, cancellationToken);
+            return await GetBytesFromStream(memoryStream, bytesRange, cancellationToken);
+        }
+
+        private async Task<byte[]> GetBytesFromStream(MemoryStream memoryStream, Range bytesRange, CancellationToken cancellationToken = default)
+        {
             memoryStream.Seek(bytesRange.Start.Value, SeekOrigin.Begin);
             var buffer = new byte[bytesRange.End.Value - bytesRange.Start.Value + 1];
             int bytesRead = await memoryStream.ReadAsync(buffer, cancellationToken);
