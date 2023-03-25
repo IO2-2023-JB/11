481639a3561315bed780353b20c7a08c9245a2ff
Marcin <marcin.latoszek06@gmail.com>
2023-03-25 17:14:57 +0100

    tests and GetVideo in controller with wrong responses

---
+++ backend/YouTubeV2.Application/Utils/RangeExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace YouTubeV2.Application.Utils
+{
+    public static class RangeExtensions
+    {
+        public static Range FromString(string input)
+        {
+            string[] range = input.Split('-');
+            if (range.Length != 2)
+                throw new ArgumentException($"Expected input argument format is int1-int2. Recieved input argument: {input}");
+            if (!int.TryParse(range[0], out int start))
+                throw new ArgumentException($"Expected input argument format is int1-int2. In recieved input ({input}) int1 can not be converted to an integer");
+            if (!int.TryParse(range[1], out int end))
+                throw new ArgumentException($"Expected input argument format is int1-int2. In recieved input ({input}) int2 can not be converted to an integer");
+            return new Range(start, end);
+        }
+    }
+}
---
+++ backend/YouTubeV2/Controllers/VideoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using YouTubeV2.Application.Services.AzureServices.BlobServices;
+using YouTubeV2.Application.Utils;
+
+namespace YouTubeV2.Api.Controllers
+{
+    [ApiController]
+    public class VideoController : ControllerBase
+    {
+        private readonly IBlobVideoService _blobVideoService;
+
+        public VideoController(IBlobVideoService blobVideoService)
+        {
+            _blobVideoService = blobVideoService;
+        }
+
+        [HttpPost("video/{id:guid}")]
+        public async Task<ActionResult<string>> GetVideoAsync(Guid id, [FromHeader] string range, CancellationToken cancellationToken)
+        {
+            Range processedRange = RangeExtensions.FromString(range);
+            byte[] bytes = await _blobVideoService.GetVideoAsync(id.ToString(), processedRange, cancellationToken);
+            return Ok(System.Text.Encoding.UTF8.GetString(bytes));
+        }
+    }
+}
