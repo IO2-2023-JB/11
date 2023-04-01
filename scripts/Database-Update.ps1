9c7412dacf7cd8dcdf08eb38b6b9e72a30abcf50
Chris <skoponer@gmail.com>
2023-04-01 17:13:19 +0200

    Some fixes

--- scripts/Database-Update.ps1
+++ scripts/Database-Update.ps1
@@ -1,6 +1,6 @@
 Push-Location -Path $PSScriptRoot\..\backend\YouTubeV2.Application
 
 dotnet ef database update --startup-project ..\YouTubeV2
-dotnet ef database update --connection "Server=localhost;User Id=sa;Password=4C0b138f;Database=YouTubeV2Test;TrustServerCertificate=True" --startup-project ..\YouTubeV2
+dotnet ef database update --connection "Server=localhost;Integrated Security = true;Database=YouTubeV2Test;TrustServerCertificate=True" --startup-project ..\YouTubeV2
 
 Pop-Location
