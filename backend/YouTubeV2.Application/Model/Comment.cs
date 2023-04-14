d206e86a7fde05d1f2efbb31baaf2d02edf1bb40
Marcin <marcin.latoszek06@gmail.com>
2023-04-14 21:35:38 +0200

    added models for comments

---
+++ backend/YouTubeV2.Application/Model/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using YouTubeV2.Application.Constants;
+
+namespace YouTubeV2.Application.Model
+{
+    public class Comment
+    {
+        public Guid Id { get; init; }
+
+        [MinLength(1)]
+        [MaxLength(CommentConstants.commentMaxLength)]
+        public string Content { get; init; } = null!;
+
+        [Required]
+        public DateTimeOffset CreateDate { get; init; }
+
+        public virtual User Author { get; init; } = null!;
+        public virtual Video Video { get; init; } = null!;
+        public virtual IReadOnlyCollection<CommentResponse> Responses { get; init; } = null!;
+    }
+}
---
+++ backend/YouTubeV2.Application/Model/CommentResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using YouTubeV2.Application.Constants;
+
+namespace YouTubeV2.Application.Model
+{
+    public class CommentResponse
+    {
+        public Guid Id { get; init; }
+
+        [MinLength(1)]
+        [MaxLength(CommentConstants.commentMaxLength)]
+        public string Content { get; init; } = null!;
+
+        [Required]
+        public DateTimeOffset CreateDate { get; init; }
+
+        public virtual User Author { get; init; } = null!;
+        public virtual Comment RespondOn { get; init; } = null!;
+    }
+}
