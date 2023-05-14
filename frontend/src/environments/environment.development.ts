93eff0e077dc37e6e1aa297f793f16ec11750085
Marcin <marcin.latoszek06@gmail.com>
2023-05-14 17:02:52 +0200

    changed environment

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
     production: false,
-    webApiUrl: 'https://wetubeback.azurewebsites.net',
-    webUrl: 'https://wetubeback.azurewebsites.net',
+    webApiUrl: 'https://localhost:7133',
+    webUrl: 'https://localhost:7133',
   };
-
+  
   /*
    * For easier debugging in development mode, you can import the following file
    * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
