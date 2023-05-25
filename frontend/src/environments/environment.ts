dfa9dc858d3afb103f587698340070c313cdbc1c
LeszekKrz <115662064+LeszekKrz@users.noreply.github.com>
2023-05-26 00:40:33 +0200

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
     production: false,
-    webApiUrl: 'https://localhost:7133',
-    webUrl: 'https://localhost:7133',
+    webApiUrl: 'https://wetubeback.azurewebsites.net/api',
+    webUrl: 'https://wetubeback.azurewebsites.net/api',
   };
   
   /*
